6de2c3c5623f2fe1f7f2ed879b1721fbe62e9bfb
Nick Entin <entin@squareup.com>
2020-05-18 16:43:34 -0700

    Add iOS 13 device to snapshot device list

--- Example/SnapshotTests/SnapshotTestCase.swift
+++ Example/SnapshotTests/SnapshotTestCase.swift
@@ -44,6 +44,7 @@ class SnapshotTestCase: FBSnapshotTestCase {
     // MARK: - Private Static Properties
 
     private static let testedDevices = [
+        TestDeviceConfig(systemVersion: "13.3", screenSize: CGSize(width: 375, height: 812), screenScale: 3),
         TestDeviceConfig(systemVersion: "12.1", screenSize: CGSize(width: 375, height: 812), screenScale: 3),
         TestDeviceConfig(systemVersion: "11.2", screenSize: CGSize(width: 375, height: 812), screenScale: 3),
     ]
