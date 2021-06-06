3b6ccf8de6e24e53b9b5cfef2829dd4c17d05ea1
Nick Entin <entin@squareup.com>
2021-06-06 16:28:03 -0700

    Verify behavior around modal accessibility view edge cases
    This verifies the (already correct) behavior around modal accessibility views to ensure that we're covering cases where:
    
    1. The view is modal and is itself an accessibility element.
    2. The view is modal and contains accessible views.
    3. The view is modal but is neither an element nor contains any elements.

--- Example/SnapshotTests/ModalTests.swift
+++ Example/SnapshotTests/ModalTests.swift
@@ -22,25 +22,61 @@ import FBSnapshotTestCase
 final class ModalTests: SnapshotTestCase {
 
     func testSingleModal() {
-        let modalAccessibilityViewController = ModalAccessibilityViewController(topLevelCount: 1, containerCount: 0)
+        let modalAccessibilityViewController = ModalAccessibilityViewController(
+            topLevelCount: 1,
+            containerCount: 0,
+            modalAccessibilityMode: .viewContainsAccessibleElement
+        )
+        modalAccessibilityViewController.view.frame = UIScreen.main.bounds
+        SnapshotVerifyAccessibility(modalAccessibilityViewController.view)
+    }
+
+    func testSingleDirectlySpecifiedModal() {
+        let modalAccessibilityViewController = ModalAccessibilityViewController(
+            topLevelCount: 1,
+            containerCount: 0,
+            modalAccessibilityMode: .viewIsAccessible
+        )
+        modalAccessibilityViewController.view.frame = UIScreen.main.bounds
+        SnapshotVerifyAccessibility(modalAccessibilityViewController.view)
+    }
+
+    func testSingleInaccessibleModal() {
+        let modalAccessibilityViewController = ModalAccessibilityViewController(
+            topLevelCount: 1,
+            containerCount: 0,
+            modalAccessibilityMode: .viewIsInaccessible
+        )
         modalAccessibilityViewController.view.frame = UIScreen.main.bounds
         SnapshotVerifyAccessibility(modalAccessibilityViewController.view)
     }
 
     func testTwoModals() {
-        let modalAccessibilityViewController = ModalAccessibilityViewController(topLevelCount: 2, containerCount: 0)
+        let modalAccessibilityViewController = ModalAccessibilityViewController(
+            topLevelCount: 2,
+            containerCount: 0,
+            modalAccessibilityMode: .viewContainsAccessibleElement
+        )
         modalAccessibilityViewController.view.frame = UIScreen.main.bounds
         SnapshotVerifyAccessibility(modalAccessibilityViewController.view)
     }
 
     func testTwoContainers() {
-        let modalAccessibilityViewController = ModalAccessibilityViewController(topLevelCount: 0, containerCount: 2)
+        let modalAccessibilityViewController = ModalAccessibilityViewController(
+            topLevelCount: 0,
+            containerCount: 2,
+            modalAccessibilityMode: .viewContainsAccessibleElement
+        )
         modalAccessibilityViewController.view.frame = UIScreen.main.bounds
         SnapshotVerifyAccessibility(modalAccessibilityViewController.view)
     }
 
     func testOneModalOneContainer() {
-        let modalAccessibilityViewController = ModalAccessibilityViewController(topLevelCount: 1, containerCount: 1)
+        let modalAccessibilityViewController = ModalAccessibilityViewController(
+            topLevelCount: 1,
+            containerCount: 1,
+            modalAccessibilityMode: .viewContainsAccessibleElement
+        )
         modalAccessibilityViewController.view.frame = UIScreen.main.bounds
         SnapshotVerifyAccessibility(modalAccessibilityViewController.view)
     }
