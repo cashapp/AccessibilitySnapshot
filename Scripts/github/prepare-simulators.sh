c6aff616d8fd8337c91e30d403fd8461cef75069
Alcivanio <alcivanio@squareup.com>
2022-09-08 10:38:33 -0400

    Update macOS 11
    Update macOS 11

--- Scripts/github/prepare-simulators.sh
+++ Scripts/github/prepare-simulators.sh
@@ -5,12 +5,13 @@ IFS=','; PLATFORMS=$(echo $1); unset IFS
 
 sudo mkdir -p /Library/Developer/CoreSimulator/Profiles/Runtimes
 
-if [[ ${PLATFORMS[*]} =~ 'iOS_13' ]]; then
-	sudo ln -s /Applications/Xcode_11.3.1.app/Contents/Developer/Platforms/iPhoneOS.platform/Library/Developer/CoreSimulator/Profiles/Runtimes/iOS.simruntime /Library/Developer/CoreSimulator/Profiles/Runtimes/iOS\ 13.3.simruntime
+
+if [[ ${PLATFORMS[*]} =~ 'iOS_14' ]]; then
+    sudo ln -s /Applications/Xcode_12.5.1.app/Contents/Developer/Platforms/iPhoneOS.platform/Library/Developer/CoreSimulator/Profiles/Runtimes/iOS.simruntime /Library/Developer/CoreSimulator/Profiles/Runtimes/iOS\ 14.5.simruntime
 fi
 
-if [[ ${PLATFORMS[*]} =~ 'iOS_12' ]]; then
-	sudo ln -s /Applications/Xcode_10.3.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/CoreSimulator/Profiles/Runtimes/iOS.simruntime /Library/Developer/CoreSimulator/Profiles/Runtimes/iOS\ 12.4.simruntime
+if [[ ${PLATFORMS[*]} =~ 'iOS_13' ]]; then
+	sudo ln -s /Applications/Xcode_11.7.app/Contents/Developer/Platforms/iPhoneOS.platform/Library/Developer/CoreSimulator/Profiles/Runtimes/iOS.simruntime /Library/Developer/CoreSimulator/Profiles/Runtimes/iOS\ 13.7.simruntime
 fi
 
 xcrun simctl list runtimes
