01c85000a0700ec226720b0b646fc3e3d260642a
James Sherlock <15193942+Sherlouk@users.noreply.github.com>
2022-10-06 10:37:45 +0100

    Update iOS Minimum Platform to iOS 13
    This is in order to support the latest version of Snapshot Testing which requires a deployment target of iOS 13 or higher. Without this change Swift Package Manager fails to resolve the dependency and thus prevents clients from using AccessibilitySnapshot with the latest versions of SnapshotTesting.

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "AccessibilitySnapshot",
     defaultLocalization: "en",
     platforms: [
-        .iOS(.v12),
+        .iOS(.v13),
     ],
     products: [
         // Core + SnapshotTesting for image comparison
