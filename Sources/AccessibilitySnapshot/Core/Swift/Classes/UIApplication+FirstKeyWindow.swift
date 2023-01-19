ca2cf861191458cbcfe37003f7dacbaeea50f281
Westin Newell <westin@squareup.com>
2023-01-19 14:25:57 -0800

    Use new API for keyWindow

---
+++ Sources/AccessibilitySnapshot/Core/Swift/Classes/UIApplication+FirstKeyWindow.swift
@@ -0,0 +1,30 @@
+//
+//  Copyright 2023 Square Inc.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import UIKit
+
+
+extension UIApplication {
+
+    public var firstKeyWindow: UIWindow? {
+        UIApplication
+            .shared
+            .connectedScenes
+            .compactMap { $0 as? UIWindowScene }
+            .flatMap { $0.windows }
+            .first { $0.isKeyWindow }
+    }
+}
