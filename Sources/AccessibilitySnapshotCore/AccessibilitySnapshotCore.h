430f1822774495d29e837c2396e9b42024ace5ee
Andrei Uriadov <andrei.uriadov@zalando.de>
2023-07-17 11:22:41 +0200

    Added carthage support for accessibility core

---
+++ Sources/AccessibilitySnapshotCore/AccessibilitySnapshotCore.h
@@ -0,0 +1,20 @@
+//
+//  AccessibilitySnapshotCore.h
+//  AccessibilitySnapshotCore
+//
+//  Created by Andrei Uriadov on 05.07.23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AccessibilitySnapshotCore.
+FOUNDATION_EXPORT double AccessibilitySnapshotCoreVersionNumber;
+
+//! Project version string for AccessibilitySnapshotCore.
+FOUNDATION_EXPORT const unsigned char AccessibilitySnapshotCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AccessibilitySnapshotCore/PublicHeader.h>
+
+#import <UIApplication+DynamicTypeSnapshotting.h>
+#import <UIView+DynamicTypeSnapshotting.h>
+#import <UIAccessibilityStatusUtility.h>
