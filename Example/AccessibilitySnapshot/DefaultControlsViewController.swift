f58bdb0468430d15c6eff19181fa2a06dff0563e
Nicholas Entin <entin@squareup.com>
2023-08-07 22:35:01 -0700

    Modernize DefaultControlsViewController

--- Example/AccessibilitySnapshot/DefaultControlsViewController.swift
+++ Example/AccessibilitySnapshot/DefaultControlsViewController.swift
@@ -19,65 +19,86 @@ import UIKit
 
 final class DefaultControlsViewController: AccessibilityViewController {
 
-    // MARK: - Private Properties
-
-    private let datePicker: UIDatePicker = .init()
-
-    private let pageControl: UIPageControl = .init()
-
-    private let segmentedControl: UISegmentedControl = .init()
-
-    private let slider: UISlider = .init()
-
-    private let stepper: UIStepper = .init()
-
-    private var controls: [UIControl] {
-        return [
-            datePicker,
-            pageControl,
-            segmentedControl,
-            slider,
-            stepper,
-        ]
-    }
-
     // MARK: - UIViewController
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        pageControl.numberOfPages = 3
-        pageControl.pageIndicatorTintColor = .lightGray
-        pageControl.currentPageIndicatorTintColor = .black
-
-        segmentedControl.insertSegment(withTitle: "Segment A", at: 0, animated: false)
-        segmentedControl.insertSegment(withTitle: "Segment B", at: 1, animated: false)
-        segmentedControl.insertSegment(withTitle: "Segment C", at: 2, animated: false)
-
-        slider.minimumValue = 0
-        slider.maximumValue = 100
-        slider.value = 75
-
-        stepper.minimumValue = -1
-        stepper.maximumValue = 1
-        stepper.value = 0
-
-        controls.forEach { view.addSubview($0) }
+    override func loadView() {
+        view = View()
     }
 
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
+}
 
-        controls.forEach { $0.sizeToFit() }
+// MARK: -
 
-        let statusBarHeight = UIApplication.shared.statusBarFrame.height
+private extension DefaultControlsViewController {
 
-        var distributionSpecifiers: [ViewDistributionSpecifying] = [ statusBarHeight.fixed, 1.flexible ]
-        for subview in controls {
-            distributionSpecifiers.append(subview)
-            distributionSpecifiers.append(1.flexible)
+    final class View: UIView {
+
+        // MARK: - Life Cycle
+
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+
+            pageControl.numberOfPages = 3
+            pageControl.pageIndicatorTintColor = .lightGray
+            pageControl.currentPageIndicatorTintColor = .black
+
+            segmentedControl.insertSegment(withTitle: "Segment A", at: 0, animated: false)
+            segmentedControl.insertSegment(withTitle: "Segment B", at: 1, animated: false)
+            segmentedControl.insertSegment(withTitle: "Segment C", at: 2, animated: false)
+
+            slider.minimumValue = 0
+            slider.maximumValue = 100
+            slider.value = 75
+
+            stepper.minimumValue = -1
+            stepper.maximumValue = 1
+            stepper.value = 0
+
+            controls.forEach { addSubview($0) }
         }
-        view.applySubviewDistribution(distributionSpecifiers)
+
+        @available(*, unavailable)
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+
+        // MARK: - Private Properties
+
+        private let datePicker: UIDatePicker = .init()
+
+        private let pageControl: UIPageControl = .init()
+
+        private let segmentedControl: UISegmentedControl = .init()
+
+        private let slider: UISlider = .init()
+
+        private let stepper: UIStepper = .init()
+
+        private var controls: [UIControl] {
+            return [
+                datePicker,
+                pageControl,
+                segmentedControl,
+                slider,
+                stepper,
+            ]
+        }
+
+        // MARK: - UIView
+
+        override func layoutSubviews() {
+            controls.forEach { $0.sizeToFit() }
+
+            let statusBarHeight = window?.windowScene?.statusBarManager?.statusBarFrame.height ?? 0
+
+            var distributionSpecifiers: [ViewDistributionSpecifying] = [ statusBarHeight.fixed, 1.flexible ]
+            for subview in controls {
+                distributionSpecifiers.append(subview)
+                distributionSpecifiers.append(1.flexible)
+            }
+            applySubviewDistribution(distributionSpecifiers)
+        }
+
     }
 
 }
