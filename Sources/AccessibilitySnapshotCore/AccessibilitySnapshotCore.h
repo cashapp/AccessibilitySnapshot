5eff4aa39f5d37769cc9f0f9b72990a18791507d
a-25 <anuryadov@gmail.com>
2023-08-10 07:04:06 +0200

    Added carthage support for accessibility core (#131)
    * Adds support for installing AccessibilitySnapshotCore through Carthage
    * Added Carthage build job to CI
    
    ---------
    
    Co-authored-by: Andrei Uriadov <andrei.uriadov@zalando.de>

---
+++ Sources/AccessibilitySnapshotCore/AccessibilitySnapshotCore.h
@@ -0,0 +1,29 @@
+//
+//  Copyright 2023 Block Inc.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AccessibilitySnapshotCore.
+FOUNDATION_EXPORT double AccessibilitySnapshotCoreVersionNumber;
+
+//! Project version string for AccessibilitySnapshotCore.
+FOUNDATION_EXPORT const unsigned char AccessibilitySnapshotCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AccessibilitySnapshotCore/PublicHeader.h>
+
+#import "UIApplication+DynamicTypeSnapshotting.h"
+#import "UIView+DynamicTypeSnapshotting.h"
+#import "UIAccessibilityStatusUtility.h"
