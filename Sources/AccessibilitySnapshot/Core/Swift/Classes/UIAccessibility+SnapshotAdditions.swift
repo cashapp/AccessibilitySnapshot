032df36b0593676ab0f55cf27884f22a65194374
Alex Odawa <aodawa@squareup.com>
2025-03-05 13:17:15 +0100

    fixing back button trait

--- Sources/AccessibilitySnapshot/Core/Swift/Classes/UIAccessibility+SnapshotAdditions.swift
+++ Sources/AccessibilitySnapshot/Core/Swift/Classes/UIAccessibility+SnapshotAdditions.swift
@@ -20,11 +20,15 @@ extension NSObject {
 
     /// Returns a tuple consisting of the `description` and (optionally) a `hint` that VoiceOver will read for the object.
     func accessibilityDescription(context: AccessibilityHierarchyParser.Context?) -> (description: String, hint: String?) {
-        var accessibilityDescription = accessibilityLabelOverride(for: context) ?? accessibilityLabel ?? ""
+        let strings = Strings(locale: accessibilityLanguage)
+
+        var accessibilityDescription =
+            accessibilityLabelOverride(for: context) ??
+        (hidesAccessibilityLabel(backDescriptor: strings.backDescriptor) ? "" :
+                                                            accessibilityLabel ?? "" )
 
         var hintDescription = accessibilityHint?.nonEmpty()
 
-        let strings = Strings(locale: accessibilityLanguage)
 
         let numberFormatter = NumberFormatter()
         if let localeIdentifier = accessibilityLanguage {
@@ -308,6 +312,12 @@ extension NSObject {
             return false
         }
     }
+    
+    private func hidesAccessibilityLabel(backDescriptor: String?) -> Bool {
+        // To prevent duplication, Back Button elements omit their label if it matches the localized "Back" descriptor.
+        ( accessibilityTraits.contains(.backButton) &&
+        (accessibilityLabel == backDescriptor) )
+    }
 
     // MARK: - Private Static Properties
 
@@ -326,6 +336,8 @@ extension NSObject {
         let buttonTraitName: String
         
         let backButtonTraitName: String
+        
+        let backDescriptor: String
 
         let tabTraitName: String
 
@@ -409,6 +421,11 @@ extension NSObject {
             self.backButtonTraitName = "Back Button.".localized(
                 key: "trait.backbutton.description",
                 comment: "Description for the 'back button' accessibility trait",
+                locale: locale
+            )
+            self.backDescriptor = "Back".localized(
+                key: "back.descriptor",
+                comment: "Descriptor for the 'back' portion of the 'back button' accessibility trait",
                 locale: locale
             )
             self.tabTraitName = "Tab.".localized(
