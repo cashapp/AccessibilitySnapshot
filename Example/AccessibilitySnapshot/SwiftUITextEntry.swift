cashapp/AccessibilitySnapshot

commit f039d27c9728266135d46f2b35eebd9ad8b618d1

Fix issue where text entry do not have "text field" or a hint in the description (#186)
Fixes the issue where text fields and text views do not have "text field" or a hint in their description

---------

Co-authored-by: David Brunow <david@shepherddog.co>
Co-authored-by: Nick Entin <nckentn@gmail.com>

Example/AccessibilitySnapshot/SwiftUITextEntry.swift
@@ -1,0 +1,36 @@
+//
+//  Copyright 2024 Square Inc.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import SwiftUI
+
+struct SwiftUITextEntry: View {
+    var body: some View {
+        VStack {
+            TextField("SwiftUI Text Field", text: .constant(""))
+            TextField("SwiftUI Text Field", text: .constant("Value in Text Field"))
+            if #available(iOS 14.0, *) {
+                TextEditor(text: .constant(""))
+            }
+        }
+    }
+}
+
+struct SwiftUITextEntry_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUITextEntry()
+            .previewLayout(.sizeThatFits)
+    }
+}

Sources/AccessibilitySnapshot/Core/Swift/Assets/de.lproj/Localizable.strings
@@ -81,3 +81,18 @@
 
 /* Description for an accessibility element indicating that it has custom actions available */
 "custom_actions.description" = "Aktionen verfügbar";
+
+/* Description for the 'text entry' accessibility trait */
+"trait.text_field.description" = "Textfeld.";
+
+/* Hint describing how to use elements with the 'text entry' accessibility trait */
+"trait.text_field.hint" = "Zum Bearbeiten doppeltippen.";
+
+/* Hint describing how to use elements with the 'text entry' accessibility trait when they are being edited */
+"trait.text_field_is_editing.hint" = "Den Rotor für den Zuriff auf „Rechtschreibfehler verwenden";
+
+/* Hint describing how to use elements with the 'text entry' and 'scrollable' accessibility traits */
+"trait.scrollable_text_field.hint" = "Zum Bearbeiten doppeltippen., Den Rotor für den Zuriff auf „Rechtschreibfehler verwenden";
+
+/* Description for the 'is editing' accessibility trait */
+"trait.text_field_is_editing.description" = "Bearbeiten";

Sources/AccessibilitySnapshot/Core/Swift/Assets/en.lproj/Localizable.strings
@@ -81,3 +81,18 @@
 
 /* Description for an accessibility element indicating that it has custom actions available */
 "custom_actions.description" = "Actions Available";
+
+/* Description for the 'text entry' accessibility trait */
+"trait.text_field.description" = "Text Field.";
+
+/* Hint describing how to use elements with the 'text entry' accessibility trait */
+"trait.text_field.hint" = "Double tap to edit.";
+
+/* Hint describing how to use elements with the 'text entry' accessibility trait when they are being edited */
+"trait.text_field_is_editing.hint" = "Use the rotor to access Misspelled Words";
+
+/* Hint describing how to use elements with the 'text entry' and 'scrollable' accessibility traits */
+"trait.scrollable_text_field.hint" = "Double tap to edit., Use the rotor to access Misspelled Words";
+
+/* Description for the 'is editing' accessibility trait */
+"trait.text_field_is_editing.description" = "Is editing.";

Sources/AccessibilitySnapshot/Core/Swift/Assets/ru.lproj/Localizable.strings
@@ -81,3 +81,18 @@
 
 /* Description for an accessibility element indicating that it has custom actions available */
 "custom_actions.description" = "Доступны действия";
+
+/* Description for the 'text entry' accessibility trait */
+"trait.text_field.description" = "текстовое поле.";
+
+/* Hint describing how to use elements with the 'text entry' accessibility trait */
+"trait.text_field.hint" = "коснитесь дважды, чтобы редактировать";
+
+/* Hint describing how to use elements with the 'text entry' accessibility trait when they are being edited */
+"trait.text_field_is_editing.hint" = "Используйте ротор для доступа к oбъeкту: Cлова с ошибками";
+
+/* Hint describing how to use elements with the 'text entry' and 'scrollable' accessibility traits */
+"trait.scrollable_text_field.hint" = "коснитесь дважды, чтобы редактировать., Используйте ротор для доступа к oбъeкту: Cлова с ошибками";
+
+/* Description for the 'is editing' accessibility trait */
+"trait.text_field_is_editing.description" = "Идет правка";

Sources/AccessibilitySnapshot/Core/Swift/Classes/UIAccessibility+SnapshotAdditions.swift
@@ -132,6 +132,20 @@ extension NSObject {
             traitSpecifiers.append(strings.tabTraitName)
         }
 
+        if accessibilityTraits.contains(.textEntry) {
+            if accessibilityTraits.contains(.scrollable) {
+                // This is a UITextView/TextEditor
+            } else {
+                // This is a UITextField/TextField
+            }
+
+            traitSpecifiers.append(strings.textEntryTraitName)
+
+            if accessibilityTraits.contains(.isEditing) {
+                traitSpecifiers.append(strings.isEditingTraitName)
+            }
+        }
+
         if accessibilityTraits.contains(.header) {
             traitSpecifiers.append(strings.headerTraitName)
         }
@@ -226,6 +240,20 @@ extension NSObject {
                 hintDescription = String(format: strings.switchButtonTraitHintFormat, existingHintDescription)
             } else {
                 hintDescription = strings.switchButtonTraitHint
+            }
+        }
+
+        if accessibilityTraits.contains(.textEntry) && !accessibilityTraits.contains(.notEnabled) {
+            if accessibilityTraits.contains(.isEditing) {
+                hintDescription = strings.textEntryIsEditingTraitHint
+            } else {
+                if accessibilityTraits.contains(.scrollable) {
+                    // This is a UITextView/TextEditor
+                    hintDescription = strings.scrollableTextEntryTraitHint
+                } else {
+                    // This is a UITextField/TextField
+                    hintDescription = strings.textEntryTraitHint
+                }
             }
         }
 
@@ -337,6 +365,16 @@ extension NSObject {
         let landmarkStartContext: String
 
         let landmarkEndContext: String
+
+        let textEntryTraitName: String
+
+        let textEntryTraitHint: String
+
+        let textEntryIsEditingTraitHint: String
+
+        let scrollableTextEntryTraitHint: String
+
+        let isEditingTraitName: String
 
         // MARK: - Life Cycle
 
@@ -479,6 +517,31 @@ extension NSObject {
                 comment: "Description of the last element in a landmark container",
                 locale: locale
             )
+            self.textEntryTraitName = "Text Field.".localized(
+                key: "trait.text_field.description",
+                comment: "Description for the 'text entry' accessibility trait",
+                locale: locale
+            )
+            self.textEntryTraitHint = "Double tap to edit.".localized(
+                key: "trait.text_field.hint",
+                comment: "Hint describing how to use elements with the 'text entry' accessibility trait",
+                locale: locale
+            )
+            self.textEntryIsEditingTraitHint = "Use the rotor to access Misspelled Words".localized(
+                key: "trait.text_field_is_editing.hint",
+                comment: "Hint describing how to use elements with the 'text entry' accessibility trait when they are being edited",
+                locale: locale
+            )
+            self.scrollableTextEntryTraitHint = "Double tap to edit., Use the rotor to access Misspelled Words".localized(
+                key: "trait.scrollable_text_field.hint",
+                comment: "Hint describing how to use elements with the 'text entry' and 'scrollable' accessibility traits",
+                locale: locale
+            )
+            self.isEditingTraitName = "Is editing.".localized(
+                key: "trait.text_field_is_editing.description",
+                comment: "Description for the 'is editing' accessibility trait",
+                locale: locale
+            )
         }
 
     }
@@ -512,6 +575,11 @@ extension UIAccessibilityTraits {
 
     static let switchButton = UIAccessibilityTraits(rawValue: 0x0020000000000000)
 
+    static let isEditing = UIAccessibilityTraits(rawValue: 0x0000000000200000)
+
+    static let textEntry = UIAccessibilityTraits(rawValue: 0x0000000000040000)
+
+    static let scrollable = UIAccessibilityTraits(rawValue: 0x0000800000000000)
 }
 
 // MARK: -

Example/AccessibilitySnapshot.xcodeproj/project.pbxproj
@@ -7,6 +7,9 @@
 	objects = {
 
 /* Begin PBXBuildFile section */
+		1104A8CF2B580AC500B6715F /* SwiftUITextEntry.swift in Sources */ = {isa = PBXBuildFile; fileRef = 1104A8CE2B580AC500B6715F /* SwiftUITextEntry.swift */; };
+		1104A8D12B595FF600B6715F /* TextFieldViewController.swift in Sources */ = {isa = PBXBuildFile; fileRef = 1104A8D02B595FF600B6715F /* TextFieldViewController.swift */; };
+		112909BC2B63E57B00B4EBEB /* TextViewViewController.swift in Sources */ = {isa = PBXBuildFile; fileRef = 112909BB2B63E57B00B4EBEB /* TextViewViewController.swift */; };
 		1635CE4E251EAC6700907101 /* SnapshotTestingTests.swift in Sources */ = {isa = PBXBuildFile; fileRef = 1635CE4D251EAC6700907101 /* SnapshotTestingTests.swift */; };
 		3D04B6D6211558B0006218A4 /* AccessibilityViewController.swift in Sources */ = {isa = PBXBuildFile; fileRef = 3D04B6D5211558B0006218A4 /* AccessibilityViewController.swift */; };
 		3D04B6D921155942006218A4 /* LabelAccessibilityPropertiesViewController.swift in Sources */ = {isa = PBXBuildFile; fileRef = 3D04B6D821155942006218A4 /* LabelAccessibilityPropertiesViewController.swift */; };
@@ -78,6 +81,9 @@
 
 /* Begin PBXFileReference section */
 		0BFCB4FD6BC17AB232B26E72 /* Pods_UnitTests.framework */ = {isa = PBXFileReference; explicitFileType = wrapper.framework; includeInIndex = 0; path = Pods_UnitTests.framework; sourceTree = BUILT_PRODUCTS_DIR; };
+		1104A8CE2B580AC500B6715F /* SwiftUITextEntry.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = SwiftUITextEntry.swift; sourceTree = "<group>"; };
+		1104A8D02B595FF600B6715F /* TextFieldViewController.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = TextFieldViewController.swift; sourceTree = "<group>"; };
+		112909BB2B63E57B00B4EBEB /* TextViewViewController.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = TextViewViewController.swift; sourceTree = "<group>"; };
 		1635CE4D251EAC6700907101 /* SnapshotTestingTests.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = SnapshotTestingTests.swift; sourceTree = "<group>"; };
 		358D84DCD315110A89BD052E /* Pods-UnitTests.debug.xcconfig */ = {isa = PBXFileReference; includeInIndex = 1; lastKnownFileType = text.xcconfig; name = "Pods-UnitTests.debug.xcconfig"; path = "Pods/Target Support Files/Pods-UnitTests/Pods-UnitTests.debug.xcconfig"; sourceTree = "<group>"; };
 		3A3192D7B9B16BD10FB517A2 /* Pods_SnapshotTests.framework */ = {isa = PBXFileReference; explicitFileType = wrapper.framework; includeInIndex = 0; path = Pods_SnapshotTests.framework; sourceTree = BUILT_PRODUCTS_DIR; };
@@ -183,6 +189,7 @@
 			children = (
 				83A295832AC22D9D00DFBE4F /* UserInputLabelsViewController.swift */,
 				3D4674672116A3F100278B57 /* ViewAccessibilityPropertiesViewController.swift */,
+				1104A8CE2B580AC500B6715F /* SwiftUITextEntry.swift */,
 				3F8E5DB82535B25000276B32 /* SwiftUIView.swift */,
 				3FEF854E253846420072611F /* SwiftUIViewWithScrollView.swift */,
 				3D39BFAB223314C1009C3EF4 /* TabBarViewController.swift */,
@@ -202,6 +209,8 @@
 				3DBAC2902242F9B200EF4D0A /* LandmarkContainerViewController.swift */,
 				3D3F2E132263E6B900F7608E /* InvertColorsViewController.swift */,
 				3DDE7FF524C6D6BF00999ABA /* AccessibilityCustomActionsViewController.swift */,
+				1104A8D02B595FF600B6715F /* TextFieldViewController.swift */,
+				112909BB2B63E57B00B4EBEB /* TextViewViewController.swift */,
 			);
 			name = "Accessibility Screens";
 			sourceTree = "<group>";
@@ -626,14 +635,17 @@
 				3D04B6D921155942006218A4 /* LabelAccessibilityPropertiesViewController.swift in Sources */,
 				3DA12A3222405B9E00EB3C33 /* DataTableViewController.swift in Sources */,
 				3DEBF24E221018610065424F /* DefaultControlsViewController.swift in Sources */,
+				1104A8D12B595FF600B6715F /* TextFieldViewController.swift in Sources */,
 				3FEF854F253846420072611F /* SwiftUIViewWithScrollView.swift in Sources */,
 				3DF464FE220D594E0048D446 /* ElementSelectionViewController.swift in Sources */,
 				3DC488372212A7D4006D1E15 /* ModalAccessibilityViewController.swift in Sources */,
 				3DF46502220D7F9B0048D446 /* ElementOrderViewController.swift in Sources */,
+				112909BC2B63E57B00B4EBEB /* TextViewViewController.swift in Sources */,
 				3DDE7FF624C6D6BF00999ABA /* AccessibilityCustomActionsViewController.swift in Sources */,
 				3DBAC28F2242E7C700EF4D0A /* ListContainerViewController.swift in Sources */,
 				3DBEAA5B2222953E00FAE61D /* SwitchControlViewController.swift in Sources */,
 				3DBAC2912242F9B200EF4D0A /* LandmarkContainerViewController.swift in Sources */,
+				1104A8CF2B580AC500B6715F /* SwiftUITextEntry.swift in Sources */,
 				3D39BFAC223314C1009C3EF4 /* TabBarViewController.swift in Sources */,
 				3D04B6DB21155D92006218A4 /* ButtonAccessibilityTraitsViewController.swift in Sources */,
 			);

Example/AccessibilitySnapshot/RootViewController.swift
@@ -14,6 +14,7 @@
 //  limitations under the License.
 //
 
+import SwiftUI
 import UIKit
 
 final class RootViewController: UITableViewController {
@@ -61,6 +62,9 @@ final class RootViewController: UITableViewController {
             ("Landmark Container", { _ in return LandmarkContainerViewController() }),
             ("Invert Colors", { _ in return InvertColorsViewController() }),
             ("User Input Labels", { _ in return UserInputLabelsViewController() }),
+            ("Text Field", { _ in return TextFieldViewController() }),
+            ("Text View", { _ in return TextViewViewController() }),
+            ("SwiftUI Text Entry", { _ in return UIHostingController(rootView: SwiftUITextEntry()) }),
         ]
 
         super.init(nibName: nil, bundle: nil)

commit 9205369bbe673aa6b08faff3377824e893898e5a

Fix mislabeled trait bit 47 (textArea) and add secureTextField (bit 24) (#327)
* Fix mislabeled private accessibility trait: bit 47 is textArea, not scrollable

Bit 47 (0x800000000000) of UIAccessibilityTraits is Apple's private
UIAccessibilityTraitTextArea, used by UITextView/TextEditor. It was
incorrectly named "scrollable" when introduced in #186. The logic was
correct (distinguishing UITextView from UITextField) but the naming was
wrong, causing incorrect serialization and misleading code semantics.

Renames the trait, updates Codable encoding, and fixes localization keys.

Co-Authored-By: Claude Opus 4.6 (1M context) <noreply@anthropic.com>

* Add secureTextField trait (bit 24) and cite AXRuntime source for private traits

Adds handling for the private secureTextField trait (bit 24,
AXTraitSecureTextField) so SecureField/secure UITextField elements are
announced as "Secure Text Field." matching VoiceOver behavior, instead of
the generic "Text Field." specifier.

Also adds a source citation comment for all private trait bit positions,
referencing AXRuntime.framework symbols as extracted in facebook/idb
PrivateHeaders/AXRuntime/AXTraits.h.

Co-Authored-By: Claude Opus 4.6 (1M context) <noreply@anthropic.com>

* Add SecureField snapshot test verifying "Secure Text Field." specifier

Adds a SwiftUISecureField demo view with empty and populated SecureField
alongside a regular TextField for comparison, and a corresponding
snapshot test. Confirms the secureTextField trait (bit 24) produces
the correct "Secure Text Field." specifier distinct from "Text Field."

Co-Authored-By: Claude Opus 4.6 (1M context) <noreply@anthropic.com>

* Add reference images for SecureField snapshot test

Co-Authored-By: Claude Opus 4.6 (1M context) <noreply@anthropic.com>

* Integrate SecureField into existing SwiftUI text entry test

Adds SecureField (empty and populated) to the SwiftUITextEntry demo view
instead of a separate file, so all text entry variants (TextField,

Example/AccessibilitySnapshot/SwiftUITextEntry.swift
@@ -5,6 +5,8 @@ struct SwiftUITextEntry: View {
         VStack {
             TextField("SwiftUI Text Field", text: .constant(""))
             TextField("SwiftUI Text Field", text: .constant("Value in Text Field"))
+            SecureField("Password", text: .constant(""))
+            SecureField("Password", text: .constant("secret123"))
             if #available(iOS 14.0, *) {
                 TextEditor(text: .constant(""))
             }

Sources/AccessibilitySnapshot/Parser/Swift/Assets/de.lproj/Localizable.strings
@@ -94,14 +94,17 @@
 /* Description for the 'text entry' accessibility trait */
 "trait.text_field.description" = "Textfeld.";
 
+/* Description for the 'secure text field' accessibility trait */
+"trait.secure_text_field.description" = "Sicheres Textfeld.";
+
 /* Hint describing how to use elements with the 'text entry' accessibility trait */
 "trait.text_field.hint" = "Zum Bearbeiten doppeltippen.";
 
 /* Hint describing how to use elements with the 'text entry' accessibility trait when they are being edited */
 "trait.text_field_is_editing.hint" = "Den Rotor für den Zuriff auf „Rechtschreibfehler verwenden";
 
-/* Hint describing how to use elements with the 'text entry' and 'scrollable' accessibility traits */
-"trait.scrollable_text_field.hint" = "Zum Bearbeiten doppeltippen., Den Rotor für den Zuriff auf „Rechtschreibfehler verwenden";
+/* Hint describing how to use elements with the 'text entry' and 'text area' accessibility traits */
+"trait.text_area.hint" = "Zum Bearbeiten doppeltippen., Den Rotor für den Zuriff auf „Rechtschreibfehler verwenden";
 
 /* Description for the 'is editing' accessibility trait */
 "trait.text_field_is_editing.description" = "Bearbeiten";

Sources/AccessibilitySnapshot/Parser/Swift/Assets/en.lproj/Localizable.strings
@@ -94,14 +94,17 @@
 /* Description for the 'text entry' accessibility trait */
 "trait.text_field.description" = "Text Field.";
 
+/* Description for the 'secure text field' accessibility trait */
+"trait.secure_text_field.description" = "Secure Text Field.";
+
 /* Hint describing how to use elements with the 'text entry' accessibility trait */
 "trait.text_field.hint" = "Double tap to edit.";
 
 /* Hint describing how to use elements with the 'text entry' accessibility trait when they are being edited */
 "trait.text_field_is_editing.hint" = "Use the rotor to access Misspelled Words";
 
-/* Hint describing how to use elements with the 'text entry' and 'scrollable' accessibility traits */
-"trait.scrollable_text_field.hint" = "Double tap to edit., Use the rotor to access Misspelled Words";
+/* Hint describing how to use elements with the 'text entry' and 'text area' accessibility traits */
+"trait.text_area.hint" = "Double tap to edit., Use the rotor to access Misspelled Words";
 
 /* Description for the 'is editing' accessibility trait */
 "trait.text_field_is_editing.description" = "Is editing.";

Sources/AccessibilitySnapshot/Parser/Swift/Assets/ru.lproj/Localizable.strings
@@ -94,14 +94,17 @@
 /* Description for the 'text entry' accessibility trait */
 "trait.text_field.description" = "текстовое поле.";
 
+/* Description for the 'secure text field' accessibility trait */
+"trait.secure_text_field.description" = "Защищённое текстовое поле.";
+
 /* Hint describing how to use elements with the 'text entry' accessibility trait */
 "trait.text_field.hint" = "коснитесь дважды, чтобы редактировать";
 
 /* Hint describing how to use elements with the 'text entry' accessibility trait when they are being edited */
 "trait.text_field_is_editing.hint" = "Используйте ротор для доступа к oбъeкту: Cлова с ошибками";
 
-/* Hint describing how to use elements with the 'text entry' and 'scrollable' accessibility traits */
-"trait.scrollable_text_field.hint" = "коснитесь дважды, чтобы редактировать., Используйте ротор для доступа к oбъeкту: Cлова с ошибками";
+/* Hint describing how to use elements with the 'text entry' and 'text area' accessibility traits */
+"trait.text_area.hint" = "коснитесь дважды, чтобы редактировать., Используйте ротор для доступа к oбъeкту: Cлова с ошибками";
 
 /* Description for the 'is editing' accessibility trait */
 "trait.text_field_is_editing.description" = "Идет правка";

Sources/AccessibilitySnapshot/Parser/Swift/Classes/AccessibilityHierarchy+Codable.swift
@@ -33,9 +33,10 @@ public extension UIAccessibilityTraits {
         // Private traits (defined in UIAccessibility+SnapshotAdditions.swift)
         (.textEntry, "textEntry"),
         (.isEditing, "isEditing"),
+        (.secureTextField, "secureTextField"),
         (.backButton, "backButton"),
         (.tabBarItem, "tabBarItem"),
-        (.scrollable, "scrollable"),
+        (.textArea, "textArea"),
         (.switchButton, "switchButton"),
     ]
 

Sources/AccessibilitySnapshot/Parser/Swift/Classes/UIAccessibility+SnapshotAdditions.swift
@@ -126,13 +126,11 @@ extension NSObject {
         }
 
         if accessibilityTraits.contains(.textEntry) {
-            if accessibilityTraits.contains(.scrollable) {
-                // This is a UITextView/TextEditor
+            if accessibilityTraits.contains(.secureTextField) {
+                traitSpecifiers.append(strings.secureTextFieldTraitName)
             } else {
-                // This is a UITextField/TextField
+                traitSpecifiers.append(strings.textEntryTraitName)
             }
-
-            traitSpecifiers.append(strings.textEntryTraitName)
 
             if accessibilityTraits.contains(.isEditing) {
                 traitSpecifiers.append(strings.isEditingTraitName)
@@ -235,9 +233,9 @@ extension NSObject {
             if accessibilityTraits.contains(.isEditing) {
                 hintDescription = strings.textEntryIsEditingTraitHint
             } else {
-                if accessibilityTraits.contains(.scrollable) {
+                if accessibilityTraits.contains(.textArea) {
                     // This is a UITextView/TextEditor
-                    hintDescription = strings.scrollableTextEntryTraitHint
+                    hintDescription = strings.textAreaTraitHint
                 } else {
                     // This is a UITextField/TextField
                     hintDescription = strings.textEntryTraitHint
@@ -366,11 +364,13 @@ extension NSObject {
 
         let textEntryTraitName: String
 
+        let secureTextFieldTraitName: String
+
         let textEntryTraitHint: String
 
         let textEntryIsEditingTraitHint: String
 
-        let scrollableTextEntryTraitHint: String
+        let textAreaTraitHint: String
 
         let isEditingTraitName: String
 
@@ -530,6 +530,11 @@ extension NSObject {
                 comment: "Description for the 'text entry' accessibility trait",
                 locale: locale
             )
+            secureTextFieldTraitName = "Secure Text Field.".localized(
+                key: "trait.secure_text_field.description",
+                comment: "Description for the 'secure text field' accessibility trait",
+                locale: locale
+            )
             textEntryTraitHint = "Double tap to edit.".localized(
                 key: "trait.text_field.hint",
                 comment: "Hint describing how to use elements with the 'text entry' accessibility trait",
@@ -540,9 +545,9 @@ extension NSObject {
                 comment: "Hint describing how to use elements with the 'text entry' accessibility trait when they are being edited",
                 locale: locale
             )
-            scrollableTextEntryTraitHint = "Double tap to edit., Use the rotor to access Misspelled Words".localized(
-                key: "trait.scrollable_text_field.hint",
-                comment: "Hint describing how to use elements with the 'text entry' and 'scrollable' accessibility traits",
+            textAreaTraitHint = "Double tap to edit., Use the rotor to access Misspelled Words".localized(
+                key: "trait.text_area.hint",
+                comment: "Hint describing how to use elements with the 'text entry' and 'text area' accessibility traits",
                 locale: locale
             )
             isEditingTraitName = "Is editing.".localized(
@@ -573,18 +578,34 @@ extension String {
 
 // MARK: -
 
+// Private UIAccessibilityTraits constants. Bit positions sourced from AXRuntime.framework
+// statics (_kAX*Trait symbols), as extracted in facebook/idb PrivateHeaders/AXRuntime/AXTraits.h.
+//
+// ┌─────┬────────────────────┬──────────────────────┬─────────────────────────────────┐
+// │ Bit │ Hex                │ Name                 │ UIKit Source                    │
+// ├─────┼────────────────────┼──────────────────────┼─────────────────────────────────┤
+// │  18 │ 0x0000000000040000 │ textEntry            │ UITextField, UITextView         │
+// │  21 │ 0x0000000000200000 │ isEditing            │ First-responder text fields     │
+// │  24 │ 0x0000000001000000 │ secureTextField      │ UITextField.isSecureTextEntry   │
+// │  27 │ 0x0000000008000000 │ backButton           │ UINavigationBar back item       │
+// │  28 │ 0x0000000010000000 │ tabBarItem           │ UITabBarButton                  │
+// │  47 │ 0x0000800000000000 │ textArea             │ UITextView, SwiftUI TextEditor  │
+// │  53 │ 0x0020000000000000 │ switchButton         │ UISwitch                        │
+// └─────┴────────────────────┴──────────────────────┴─────────────────────────────────┘
 extension UIAccessibilityTraits {
-    static let textEntry = UIAccessibilityTraits(rawValue: 1 << 18) // 0x0000000000040000
+    static let textEntry = UIAccessibilityTraits(rawValue: 1 << 18)
 
-    static let isEditing = UIAccessibilityTraits(rawValue: 1 << 21) // 0x0000000000200000
+    static let isEditing = UIAccessibilityTraits(rawValue: 1 << 21)
 
-    static let backButton = UIAccessibilityTraits(rawValue: 1 << 27) // 0x0000000008000000
+    static let secureTextField = UIAccessibilityTraits(rawValue: 1 << 24)
 
-    static let tabBarItem = UIAccessibilityTraits(rawValue: 1 << 28) // 0x0000000010000000
+    static let backButton = UIAccessibilityTraits(rawValue: 1 << 27)
 
-    static let scrollable = UIAccessibilityTraits(rawValue: 1 << 47) // 0x0000800000000000
+    static let tabBarItem = UIAccessibilityTraits(rawValue: 1 << 28)
 
-    static let switchButton = UIAccessibilityTraits(rawValue: 1 << 53) // 0x0020000000000000
+    static let textArea = UIAccessibilityTraits(rawValue: 1 << 47)
+
+    static let switchButton = UIAccessibilityTraits(rawValue: 1 << 53)
 }
 
 // MARK: -