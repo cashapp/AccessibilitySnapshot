RoyalPineapple/AccessibilitySnapshotBH

Scope tab bar memoization cache to parse call lifetime
viewToElementsMap was an instance property on AccessibilityHierarchyParser
that cached [UIView: [NSObject]] for tab bar context resolution. Since
TheBurglar holds a single parser instance for the app's lifetime, every
tab bar view ever parsed — and all its child NSObjects — stayed alive
forever. Over long benchmark runs this accumulated tens of GB.

Replace the instance property with a local variable passed as an inout
parameter to context(). The cache still avoids redundant tab bar parsing
within a single parse call, but the dictionary dies when the function
returns — no strong refs outlive the parse.

Co-Authored-By: Claude Opus 4.6 (1M context) <noreply@anthropic.com>

Sources/AccessibilitySnapshot/Parser/Swift/Classes/AccessibilityHierarchyParser.swift
@@ -184,6 +184,7 @@ public final class AccessibilityHierarchyParser {
             userInterfaceIdiom: userInterfaceIdiom
         )
 
+        var tabBarCache: [UIView: [NSObject]] = [:]
         let contextualizedElements = uncontextualizedElements.map { element in
             ContextualElement(
                 object: element.object,
@@ -191,7 +192,8 @@ public final class AccessibilityHierarchyParser {
                     for: element.object,
                     from: element.contextProvider,
                     userInterfaceLayoutDirection: userInterfaceLayoutDirection,
-                    userInterfaceIdiom: userInterfaceIdiom
+                    userInterfaceIdiom: userInterfaceIdiom,
+                    tabBarCache: &tabBarCache
                 )
             )
         }
@@ -343,7 +345,8 @@ public final class AccessibilityHierarchyParser {
         for element: NSObject,
         from contextProvider: ContextProvider?,
         userInterfaceLayoutDirection: UIUserInterfaceLayoutDirection,
-        userInterfaceIdiom: UIUserInterfaceIdiom
+        userInterfaceIdiom: UIUserInterfaceIdiom,
+        tabBarCache: inout [UIView: [NSObject]]
     ) -> Context? {
         guard let contextProvider = contextProvider else {
             return nil
@@ -385,7 +388,7 @@ public final class AccessibilityHierarchyParser {
             // hierarchy under the view are treated as tabs.
             if view.accessibilityTraits.contains(.tabBar), let element = element as? UIView {
                 let accessibleElements: [NSObject]
-                if let elements = viewToElementsMap[view] {
+                if let elements = tabBarCache[view] {
                     accessibleElements = elements
                 } else {
                     let hierarchy = view.recursiveAccessibilityHierarchy()
@@ -396,7 +399,7 @@ public final class AccessibilityHierarchyParser {
                         userInterfaceLayoutDirection: userInterfaceLayoutDirection,
                         userInterfaceIdiom: userInterfaceIdiom
                     ).map { $0.object }
-                    viewToElementsMap[view] = accessibleElements
+                    tabBarCache[view] = accessibleElements
                 }
 
                 return .tab(
@@ -514,9 +517,6 @@ public final class AccessibilityHierarchyParser {
     }
 
     // MARK: - Private Properties
-
-    /// Used for memoization of accessibility hierarchy parsing when determining element contexts.
-    private var viewToElementsMap: [UIView: [NSObject]] = [:]
 
     // MARK: - Private Hierarchy Methods
 

cashapp/AccessibilitySnapshot

Sources/AccessibilitySnapshot/Parser/Swift/Classes/AccessibilityHierarchyParser.swift
@@ -160,6 +160,7 @@ public final class AccessibilityHierarchyParser {
             userInterfaceIdiom: userInterfaceIdiom
         )
 
+        var viewToElementsMap: [UIView: [NSObject]] = [:]
         let contextualizedElements = uncontextualizedElements.map { element in
             ContextualElement(
                 object: element.object,
@@ -167,7 +168,8 @@ public final class AccessibilityHierarchyParser {
                     for: element.object,
                     from: element.contextProvider,
                     userInterfaceLayoutDirection: userInterfaceLayoutDirection,
-                    userInterfaceIdiom: userInterfaceIdiom
+                    userInterfaceIdiom: userInterfaceIdiom,
+                    viewToElementsMap: &viewToElementsMap
                 )
             )
         }
@@ -315,7 +317,8 @@ public final class AccessibilityHierarchyParser {
         for element: NSObject,
         from contextProvider: ContextProvider?,
         userInterfaceLayoutDirection: UIUserInterfaceLayoutDirection,
-        userInterfaceIdiom: UIUserInterfaceIdiom
+        userInterfaceIdiom: UIUserInterfaceIdiom,
+        viewToElementsMap: inout [UIView: [NSObject]]
     ) -> Context? {
         guard let contextProvider = contextProvider else {
             return nil
@@ -486,9 +489,6 @@ public final class AccessibilityHierarchyParser {
     }
 
     // MARK: - Private Properties
-
-    /// Used for memoization of accessibility hierarchy parsing when determining element contexts.
-    private var viewToElementsMap: [UIView: [NSObject]] = [:]
 
     // MARK: - Private Hierarchy Methods
 