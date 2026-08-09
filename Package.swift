350255e494904ef88bfd0cee4711255a792f9e9c
Enrique Garcia <github@lemambo.com>
2026-08-09 14:14:00 +0200

    Bump swift-tools-version to 5.9 and declare visionOS platform

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -9,6 +9,7 @@ let package = Package(
     platforms: [
         .iOS(.v13),
         .macOS(.v10_15),
+        .visionOS(.v1),
     ],
     products: [
         // Core + SnapshotTesting for image comparison
@@ -39,12 +40,10 @@ let package = Package(
     dependencies: [
         .package(path: "AccessibilitySnapshotModel"),
         .package(
-            name: "iOSSnapshotTestCase",
             url: "https://github.com/uber/ios-snapshot-test-case.git",
             .upToNextMajor(from: "8.0.0")
         ),
         .package(
-            name: "SnapshotTesting",
             url: "https://github.com/pointfreeco/swift-snapshot-testing.git",
             .upToNextMajor(from: "1.10.0")
         ),
@@ -80,7 +79,7 @@ let package = Package(
                 "AccessibilitySnapshotCore",
                 "AccessibilitySnapshotParser-ObjC",
                 "AccessibilitySnapshotPreviews",
-                "SnapshotTesting",
+                .product(name: "SnapshotTesting", package: "swift-snapshot-testing"),
             ],
             path: "Sources/AccessibilitySnapshot/SnapshotTesting"
         ),
@@ -90,13 +89,17 @@ let package = Package(
                 "AccessibilitySnapshotCore",
                 "AccessibilitySnapshotParser-ObjC",
                 "AccessibilitySnapshotPreviews",
-                "iOSSnapshotTestCase",
+                .product(name: "iOSSnapshotTestCase", package: "ios-snapshot-test-case"),
             ],
             path: "Sources/AccessibilitySnapshot/iOSSnapshotTestCase/Swift"
         ),
         .target(
             name: "FBSnapshotTestCase-Accessibility-ObjC",
-            dependencies: ["AccessibilitySnapshotCore", "iOSSnapshotTestCase", "FBSnapshotTestCase-Accessibility"],
+            dependencies: [
+                "AccessibilitySnapshotCore",
+                .product(name: "iOSSnapshotTestCase", package: "ios-snapshot-test-case"),
+                "FBSnapshotTestCase-Accessibility",
+            ],
             path: "Sources/AccessibilitySnapshot/iOSSnapshotTestCase/ObjC"
         ),
     ]
